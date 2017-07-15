7e07bca23ab70158fbf3d393a2abf7c17f9e47f0
Omkar Mohite <omkarmohite310@gmail.com>
2017-07-15 17:19:39 +0530

    Update clusterMinSet.m

--- clusterMinSet.m
+++ clusterMinSet.m
@@ -1,9 +1,9 @@
-function [Cluster,nClust] = clusterMinSet( model, Jdl, minRerouted )
+function [Cluster] = clusterMinSet( model, Jdl, minRerouting )
 %Clustering of lethals based on minRerouting sets
 p=zeros(length(Jdl),length(model.rxns));
 for iLeth=1:length(Jdl)
-    if ~isempty(minRerouted(iLeth).rxns)
-        p(iLeth,find(ismember(model.rxns,minRerouted(iLeth).rxns)))=1;
+    if ~isempty(minRerouting(iLeth).rxns)
+        p(iLeth,find(ismember(model.rxns,minRerouting(iLeth).rxns)))=1;
     end
 end    
 D=pdist(p,'jaccard');
@@ -14,11 +14,13 @@ for k=1:max(c)
     if(sum(c==k)<=1)
         continue
     end
-    fprintf('Cluster %d:\n',k)
-    Cluster(m).Jdl=Jdl(c==k,:); m=m+1;
-    Jdl(c==k,:)
-    unique(sum(p(find(c==k),:),2))
+%     fprintf('Cluster %d:\n',k)
+    Cluster_temp(m).Jdl=Jdl(c==k,:); 
+    len(m)=length(Jdl(c==k,:)); m=m+1;
 end
-nClust=length(Cluster);
+[Y,I]=sort(len,'descend');
+for Idx=1:m-1
+    Cluster(Idx).Jdl=Cluster_temp(I(Idx)).Jdl;
 end
-
+fprintf('Total number of clusters identified: %d\n',m-1)
+end
