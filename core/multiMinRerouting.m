20d35a374e7bbae4a8a62ffa383e45aa42046714
Omkar Mohite <omkarmohite310@gmail.com>
2018-09-01 15:46:35 +0200

    Update multiMinRerouting.m

--- core/multiMinRerouting.m
+++ core/multiMinRerouting.m
@@ -1,16 +1,22 @@
-function [Castle] = multiMinRerouting(Castle)
-%MULTIMINREROUTING Summary of this function goes here
-%   Detailed explanation goes here
+function [Castle] = multiMinRerouting(Castle, minNorm)
+% [Castle] = multiMinRerouting(Castle, minNorm) 
+% multiMinRerouting identifies minimal rerouting sets for synthetic lethals of multiple models in Castle structure 
+%   Input:
+%   Castle : The structure with models and synthetic lethals data
+%   minNorm : 'two', 'one' or 'zero' identifies minReroutingSets using minimal L2, L1 or L0 norm formulation of LP
+
 nModels = length(Castle.data);
 for j= 1:nModels
-    disp("Finding set of minimal rerouting reactions for double lethals in " + Castle.data(j).model_name);
-%     Castle.data(j).minRe =
-%     minReroutingRxns(Castle.data(j).model,Castle.data(j).Jdl); % Old
-%     version
-%    Castle.data(j).minRe = minReroutingRxns_l2_moma(Castle.data(j).model,Castle.data(j).Jdl);    %quadraticMOMA version
-%    Castle.data(j).minRe = minReroutingRxns_l1_moma(Castle.data(j).model,Castle.data(j).Jdl);    %linearMOMA version
-   Castle.data(j).minRe = minReroutingRxns_l0_moma(Castle.data(j).model,Castle.data(j).Jdl);    %sparseMOMA version
-
+    if minNorm == 'two'
+        disp("Finding set of minimal rerouting reactions using L2 minNorm in " + Castle.data(j).model_name);
+        Castle.data(j).minRe = minReroutingRxns_l2_moma(Castle.data(j).model,Castle.data(j).Jdl);    %quadraticMOMA version    
+    elif minNorm == 'one'
+        disp("Finding set of minimal rerouting reactions using L1 minNorm in " + Castle.data(j).model_name);
+        Castle.data(j).minRe = minReroutingRxns_l1_moma(Castle.data(j).model,Castle.data(j).Jdl);    %linearMOMA version   
+    elif minNorm == 'zero
+        disp("Finding set of minimal rerouting reactions using L0 minNorm (iterative formulation) in " + Castle.data(j).model_name);
+        Castle.data(j).minRe = minReroutingRxns(Castle.data(j).model,Castle.data(j).Jdl); % Iterative formulation    
+    end
 end    
 end
 
