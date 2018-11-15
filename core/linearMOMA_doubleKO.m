bd0a33656a0ea8eb39f159413d5cc564e7123d5d
omkarmohite310 <omkmoh@biosutain.dtu.dk>
2018-11-15 15:54:41 +0100

    L1_minimal slack formulation

--- core/linearMOMA_doubleKO.m
+++ core/linearMOMA_doubleKO.m
@@ -148,7 +148,7 @@ if (solutionDel1.stat > 0 && solutionDel2.stat > 0)
          sparse(1,nRxns1) modelDel2.c' sparse(1,2*nCommon)];
 
     % Construct the RHS vector
-    b = [zeros(nMets1+nMets2+2*nCommon,1);0.05*grWT; 0.05*grWT];
+    b = [zeros(nMets1+nMets2+2*nCommon,1);0.95*grWT; 0.95*grWT];
 
     % Construct the objective (sum of all delta+ and delta-)
     c = [zeros(nRxns1+nRxns2,1);ones(2*nCommon,1)];
@@ -220,7 +220,7 @@ if (solutionDel1.stat > 0 && solutionDel2.stat > 0)
             sparse(1,nRxns1) modelDel2.c' sparse(1,2*nCommon+2*nRxns1+2*nRxns2);
             sparse(1,nRxns1+nRxns2) ones(1,2*nCommon) sparse(1,2*nRxns1+2*nRxns2)];
         % Construct the RHS vector
-        b = [zeros(nMets1+nMets2+2*nCommon+2*nRxns1+2*nRxns2,1); 0.05*grWT;0.05*grWT;ceil(totalFluxDiff/tol)*tol];
+        b = [zeros(nMets1+nMets2+2*nCommon+2*nRxns1+2*nRxns2,1); 0.95*grWT;0.95*grWT;ceil(totalFluxDiff/tol)*tol];
 
         % Construct the objective (sum of all delta+ and delta-)
         c = [zeros(nRxns1+nRxns2+2*nCommon,1);ones(2*nRxns1+2*nRxns2,1)];
